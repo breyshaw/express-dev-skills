d137a642e3a6bc213e2578ce7c6e51319d794aaa
breyshaw <dawsonbrey@gmail.com>
2021-10-27 21:57:02 -0400

    finish delete method

--- controllers/skills.js
+++ controllers/skills.js
@@ -12,29 +12,36 @@ function index(req, res) {
     })
 }
 
-function show(req,res) {
+function show(req, res) {
     skillDb.findById(req.params.id, function (error, skill) {
         res.render('skills/show', {
             skill: skill,
             error: error
         })
-    }) 
+    })
 }
 
-function newSkill(req,res) {
+function newSkill(req, res) {
     res.render('skills/new')
 }
 
-function create(req,res) {
- skillDb.create(req.body, function(error, skill) {
-     res.redirect('/skills')
- })
+function create(req, res) {
+    skillDb.create(req.body, function (error, skill) {
+        res.redirect('/skills')
+    })
 
+}
+
+function deleteSkill(req, res) {
+    skillDb.findByIdAndDelete(req.params.id, function (error, skill) {
+        res.redirect('/skills')
+    })
 }
 
 export {
     index,
     show,
     newSkill as new,
-    create
+    create,
+    deleteSkill as delete
 }
